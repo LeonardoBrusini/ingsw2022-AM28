9c9d3472b51bec46fd779a2239790982d769a5ba
LorenzoBiasiolo <100306570+LorenzoBiasiolo@users.noreply.github.com>
2022-03-23 21:56:03 +0100

    Add files via upload

---
+++ ProfessorGroup.java
@@ -0,0 +1,34 @@
+package com.example.prova1;
+
+import java.util.ArrayList;
+import java.util.EnumMap;
+
+public class ProfessorGroup {
+    private EnumMap<Colour, Tower> professors;
+
+    public ProfessorGroup(){
+        this.professors = new EnumMap<Colour,Tower>(Colour.class);
+    }
+
+    public Tower getTower(Colour colour){
+        return this.professors.get(colour);
+    }
+
+    public void setTower(Colour colour,Tower tower){
+        this.professors.put(colour,tower);
+    }
+
+    public ArrayList<Colour> getColours(Tower t){
+        ArrayList<Colour> ret = new ArrayList<>();
+        for(Colour c: Colour.values())
+            if(t.equals(this.professors.get(c)))
+                ret.add(c);
+
+        return ret;
+    }
+
+
+
+
+
+}
