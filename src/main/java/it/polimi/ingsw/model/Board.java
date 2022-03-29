495fcecd63b5377520469ca0de1215b6e6d48b4d
LorenzoBiasiolo <lorenzo.biasiolo@mail.polimi.it>
2022-03-29 16:54:25 +0200

    Ho pensato di aggiungere un ulteriore remove student che fa l'opposto di addStudent

--- src/main/java/it/polimi/ingsw/model/Board.java
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -5,26 +5,25 @@ import java.util.ArrayList;
 public class Board {
     private int coins;
     private int numPlayers;
-    private ArrayList<Cloud> clouds;
-    private ArrayList<AbstractIsland> islands;
+    private ArrayList<Cloud> clouds = new ArrayList<>();
+    private ArrayList<AbstractIsland> islands = new ArrayList<>();
     private MotherNature motherNature;
-    private Bag bag;
+    private Bag bag = new Bag();
 
     public Board(int numPlayers){
         this.coins = 20 - numPlayers;
-        this.bag = new Bag();
         this.numPlayers = numPlayers;
-        this.clouds = new ArrayList<>();
-        for(int i = 0 ;i<numPlayers ; i++){
+        int i;
+        for(i = 0 ;i<numPlayers ; i++){
             this.clouds.get(i).setGroup(this.bag.removeStudent(4));
         }
 
         this.motherNature = new MotherNature();
 
-        this.islands = new ArrayList<>();
-        for(int i = 0; i< 12; i++){
+        for(i = 0; i < 12; i++){
+            this.islands.get(i) = new Island(i);
             this.islands.get(i).setPosIndex(i);
-            if(i == motherNature.getIslandIndex() || (motherNature.getIslandIndex()+ 6 % 12)==i ){
+            if(i == motherNature.getIslandIndex() || (motherNature.getIslandIndex()+ 6 % 12)==i){
                 this.islands.get(i).setPresenceMotherNature(i == motherNature.getIslandIndex());
                 StudentGroup c = new StudentGroup(this.bag.removeStudent(1));
                 this.islands.get(i).setStudentGroup(c);
@@ -34,6 +33,7 @@ public class Board {
             }
         }
     }
+
 
 
 
