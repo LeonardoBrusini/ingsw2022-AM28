9c2f79d1a8a7b18a6b71159d7c9084b76f83f39e
Isaia Belardinelli <isaia.belardinelli@mail.polimi.it>
2022-04-12 10:42:00 +0200

    playerWithMostInfluence test added in MotherNatureTest.java

--- src/test/java/it/polimi/ingsw/model/board/MotherNatureTest.java
+++ src/test/java/it/polimi/ingsw/model/board/MotherNatureTest.java
@@ -22,7 +22,7 @@ class MotherNatureTest {
         int res = m.getIslandIndex();
         m.setIsland(res);
         int con = m.getIslandIndex();
-        assertEquals(res,con);
+        assertEquals(res, con);
     }
 
     @Test
@@ -30,23 +30,41 @@ class MotherNatureTest {
         m = new MotherNature();
         m.setIsland(12);
 
-        assertEquals(12,m.getIslandIndex());
+        assertEquals(12, m.getIslandIndex());
 
     }
-    /*
+
     @Test
-    void playerWithMostInfluence(){
+    void playerWithMostInfluence() {
+        MotherNature mn = new MotherNature();
+        mn.setIsland(3);
         IslandManager im = new IslandManager(3);
-        //im.getArchipelagos().get(2).merge(im.getArchipelagoByIslandIndex(3));
+        ArrayList<Player> players = new ArrayList<>();
+        StudentGroup st = new StudentGroup(2);
+        st.addStudent(Colour.BLUE);
+        im.getIsland(3).setStudents(st);
         EnumMap<Colour, Tower> en = new EnumMap<>(Colour.class);
         ProfessorGroup pg = new ProfessorGroup();
+        Player p1 = new Player("g1", Tower.BLACK);
+        Player p2 = new Player("g2", Tower.WHITE);
+        players.add(p1);
+        players.add(p2);
+        assertNull(mn.playerWithMostInfluence(players,im,pg));
+
         en.put(Colour.RED, Tower.BLACK);
+        en.put(Colour.BLUE, Tower.BLACK);
         en.put(Colour.YELLOW, Tower.WHITE);
         pg.setProfessors(en);
-        im.getIsland(3).setStudents(new StudentGroup(new Bag().removeStudentGroup(5)));
-        ArrayList<Player> players = new ArrayList<>();
-        players.add(new Player("g1"));
-        players.add(new Player("g2"));
+        assertEquals(p1, mn.playerWithMostInfluence(players, im, pg));
 
-    }*/
+        im.getArchipelagos().get(2).merge(im.getArchipelagoByIslandIndex(3));
+        assertEquals(players.get(0), mn.playerWithMostInfluence(players, im, pg));
+
+        Player p3 = new Player("g3", Tower.GRAY);
+        players.add(p3);
+        en.put(Colour.BLUE, Tower.GRAY);
+        pg.setProfessors(en);
+        assertEquals(p3, mn.playerWithMostInfluence(players, im, pg));
+
+    }
 }
