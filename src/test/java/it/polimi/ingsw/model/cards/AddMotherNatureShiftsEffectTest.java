db2a02440b261ceffa087ee0d8087004b4012d93
Isaia Belardinelli <isaia.belardinelli@mail.polimi.it>
2022-04-14 18:16:33 +0200

    NoEntryTileEffectTest

--- src/test/java/it/polimi/ingsw/model/cards/AddMotherNatureShiftsEffectTest.java
+++ src/test/java/it/polimi/ingsw/model/cards/AddMotherNatureShiftsEffectTest.java
@@ -1,16 +1,33 @@
 package it.polimi.ingsw.model.cards;
 
+import it.polimi.ingsw.controller.ExpertGameManager;
+import it.polimi.ingsw.model.players.AssistantCard;
+import it.polimi.ingsw.model.players.Player;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddMotherNatureShiftsEffectTest {
-    @Test
+    /*@Test
     void resolveEffect() {
-        /*AssistantCard as = c.getPlayerThisTurn().getLastPlayedCard();
-        int w = as.getMotherNatureShifts();
-        //resolveEffect(c);
-        assertEquals(w + 2, as.getMotherNatureShifts());*/
-    }
+        ExpertGameManager gm = new ExpertGameManager();
+        int moves;
+        gm.newGame();
+        gm.addPlayer("g1");
+        gm.addPlayer("g1");
+        CharacterCard c = new CharacterCard(CharacterCardsInfo.CARD4);
+        c.setPlayerThisTurn(gm.getPlayers().get(0));
+        for(int i = 0; i < 10; i++){
+            c.getPlayerThisTurn().playCard(i);
+            moves = c.getPlayerThisTurn().getLastPlayedCard().getMotherNatureShifts();
+            assertEquals(i,c.getPlayerThisTurn().getLastPlayedCard());
+            c.getCardInfo().getEffect().resolveEffect(c);
+            moves += 2;
+            assertEquals(moves, c.getPlayerThisTurn().getLastPlayedCard().getMotherNatureShifts());
+        }
+    }*/
 
 }
--- src/test/java/it/polimi/ingsw/model/cards/NoEntryTileEffectTest.java
+++ src/test/java/it/polimi/ingsw/model/cards/NoEntryTileEffectTest.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.model.cards;
 
+import it.polimi.ingsw.model.board.Archipelago;
+import it.polimi.ingsw.model.board.Board;
+import it.polimi.ingsw.model.board.Island;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,15 +10,40 @@ import static org.junit.jupiter.api.Assertions.*;
 class NoEntryTileEffectTest {
     @Test
     void resolveEffect() {
-        /*Archipelago a = Board.instance().getIslandManager().getArchipelagoByIslandIndex(c.getSelectedIsland().getIslandIndex());
+        CharacterCard c = new CharacterCard(CharacterCardsInfo.CARD5);
+        c.setBoard(new Board(2));
+        c.setSelectedIsland(c.getBoard().getIslandManager().getIsland(3));
+        Archipelago a = c.getBoard().getIslandManager().getArchipelagoByIslandIndex(c.getSelectedIsland().getIslandIndex());
+
+        c.setNoEntryTiles(1);
+        a.setNoEntryTiles(3);
         int anet = a.getNoEntryTiles();
         int cnet = c.getNoEntryTiles();
-        if(c.getNoEntryTiles()>0){
-            assertEquals(anet + 1,a.getNoEntryTiles());
-            assertEquals(cnet - 1, c.getNoEntryTiles());
-        }else{
-            assertEquals(anet ,a.getNoEntryTiles());
-            assertEquals(cnet , c.getNoEntryTiles());
-        }*/
+        c.getCardInfo().getEffect().resolveEffect(c);
+        //removed if cycle to test all cases indivually
+
+        assertEquals(anet + 1, a.getNoEntryTiles());
+        assertEquals(cnet - 1, c.getNoEntryTiles());
+
+        c.setNoEntryTiles(2);
+        a.setNoEntryTiles(3);
+        int anet1 = a.getNoEntryTiles();
+        int cnet1 = c.getNoEntryTiles();
+        c.getCardInfo().getEffect().resolveEffect(c);
+        //removed if cycle to test all cases individually
+
+        assertEquals(anet1 + 1, a.getNoEntryTiles());
+        assertEquals(cnet1 - 1, c.getNoEntryTiles());
+
+        c.setNoEntryTiles(0);
+        a.setNoEntryTiles(3);
+        int anet2 = a.getNoEntryTiles();
+        int cnet2 = c.getNoEntryTiles();
+        c.getCardInfo().getEffect().resolveEffect(c);
+        //removed if cycle to test all cases indivually
+
+        assertEquals(anet2 , a.getNoEntryTiles());
+        assertEquals(cnet2 , c.getNoEntryTiles());
+
     }
 }
