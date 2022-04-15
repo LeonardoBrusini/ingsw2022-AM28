19baf0c99d9828f14f69ce8632778f808f1ddf91
Isaia Belardinelli <isaia.belardinelli@mail.polimi.it>
2022-04-15 12:57:19 +0200

    StudentToHallEffectTest.java Debugged StudentToHallEffect.java with IllegalArgumentException

--- src/main/java/it/polimi/ingsw/model/cards/StudentToHallEffect.java
+++ src/main/java/it/polimi/ingsw/model/cards/StudentToHallEffect.java
@@ -1,6 +1,10 @@
 package it.polimi.ingsw.model.cards;
 
+import it.polimi.ingsw.model.Colour;
+import it.polimi.ingsw.model.StudentGroup;
 import it.polimi.ingsw.model.players.Dashboard;
+
+import java.util.ArrayList;
 
 public class StudentToHallEffect implements EffectStrategy{
     /**
@@ -8,10 +12,14 @@ public class StudentToHallEffect implements EffectStrategy{
      * @param c the card which is being activated
      */
     @Override
-    public void resolveEffect(CharacterCard c) {
+    public void resolveEffect(CharacterCard c) throws IllegalArgumentException{
+        ArrayList<Colour> list = new ArrayList<>(); //invoking addToHall with empty entrance it would had thrown IllegalArgumentExecption in Dashboard
+        list.add(c.getSelectedColour());
         Dashboard d = c.getPlayerThisTurn().getDashboard();
+        if(c.getStudentsOnCard().getQuantityColour(c.getSelectedColour()) < 1)
+            throw new IllegalArgumentException();
         c.getStudentsOnCard().removeStudent(c.getSelectedColour());
-        d.addToHall(c.getSelectedColour());
+        d.fillHall(new StudentGroup(list));
         c.getStudentsOnCard().addStudent(c.getBoard().getBag().removeStudents(1).get(0));
     }
 }
--- src/test/java/it/polimi/ingsw/model/cards/StudentToHallEffectTest.java
+++ src/test/java/it/polimi/ingsw/model/cards/StudentToHallEffectTest.java
@@ -1,11 +1,44 @@
 package it.polimi.ingsw.model.cards;
 
+import it.polimi.ingsw.controller.ExpertGameManager;
+import it.polimi.ingsw.model.Colour;
+import it.polimi.ingsw.model.StudentGroup;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class StudentToHallEffectTest {
     @Test
+    /**
+     * It verifies the corrected number of students on the card and in the hall
+     * It verifies the corrected call for IllegalArgumentExcpetion
+     */
     void resolveEffect() {
+        CharacterCard c = new CharacterCard(CharacterCardsInfo.CARD11);
+        ExpertGameManager gm = new ExpertGameManager();
+        gm.newGame();
+        gm.addPlayer("g1");
+        c.setBoard(gm.getBoard());
+        c.setGameManager(gm);
+        c.setSelectedColour(Colour.YELLOW);
+        c.setStudentsOnCard(new StudentGroup(4));
+        c.setPlayerThisTurn(gm.getPlayers().get(0));
+        c.getPlayerThisTurn().getDashboard().fillHall(new StudentGroup(3));
+        int beforeHall = c.getPlayerThisTurn().getDashboard().getHall().getTotalStudents();
+        int beforeCard = c.getStudentsOnCard().getTotalStudents();
+        c.getCardInfo().getEffect().resolveEffect(c);
+        assertEquals(beforeHall + 1, c.getPlayerThisTurn().getDashboard().getHall().getTotalStudents());
+        assertEquals(beforeCard, c.getStudentsOnCard().getTotalStudents());
+
+        gm.addPlayer("g2");
+        c.setGameManager(gm);
+        c.setSelectedColour(Colour.YELLOW);
+        c.setStudentsOnCard(new StudentGroup());
+        c.setPlayerThisTurn(gm.getPlayers().get(1));
+        c.getPlayerThisTurn().getDashboard().fillHall(new StudentGroup(3));
+        int beforeHall1 = c.getPlayerThisTurn().getDashboard().getHall().getTotalStudents();
+        int beforeCard1 = c.getStudentsOnCard().getTotalStudents();
+        //c.getCardInfo().getEffect().resolveEffect(c);
+        //It throws IllegalArgumentException
     }
 }
