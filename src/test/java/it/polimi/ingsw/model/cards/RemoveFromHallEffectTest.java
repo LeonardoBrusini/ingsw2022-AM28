e1510a95a84fede00feab082e9971e524ff2e30b
LeonardoBrusini <leonardo.brusini@mail.polimi.it>
2022-04-20 10:20:07 +0200

    fixed RemoveFromHallEffectTest.

--- src/test/java/it/polimi/ingsw/model/cards/RemoveFromHallEffectTest.java
+++ src/test/java/it/polimi/ingsw/model/cards/RemoveFromHallEffectTest.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RemoveFromHallEffectTest {
-    
+
     /**
      * It verifies that each Hall has the correct number of students after the card's effect execution
      */
