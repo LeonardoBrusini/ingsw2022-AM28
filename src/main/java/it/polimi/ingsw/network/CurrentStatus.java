23de6b21bb5e3c4e21004e0347b9644d141a218a
Isaia Belardinelli <isaia.belardinelli@mail.polimi.it>
2022-05-01 19:50:05 +0200

    CurrentStatus & GameStatus setters, MoveMotherNatureCommand update

--- src/main/java/it/polimi/ingsw/network/CurrentStatus.java
+++ src/main/java/it/polimi/ingsw/network/CurrentStatus.java
@@ -9,4 +9,25 @@ public class CurrentStatus {
     String winner;
     TurnStatus turn;
     GameStatus game;
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public void setWinner(String winner) {
+        this.winner = winner;
+    }
+
+    public void setTurn(TurnStatus turn) {
+        this.turn = turn;
+    }
+
+    public void setGame(GameStatus game) {
+        this.game = game;
+    }
+
 }
