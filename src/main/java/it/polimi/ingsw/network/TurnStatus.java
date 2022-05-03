1c90d2450cd3dfb67f52214d072a27105f3cb8eb
LorenzoBiasiolo <lorenzo.biasiolo@mail.polimi.it>
2022-05-03 07:18:58 +0200

    TurnStatus

--- src/main/java/it/polimi/ingsw/network/TurnStatus.java
+++ src/main/java/it/polimi/ingsw/network/TurnStatus.java
@@ -4,6 +4,23 @@ package it.polimi.ingsw.network;
  * It represents the current status of the Turn during the match
  */
 public class TurnStatus {
-    int player;
-    String phase;
+    private int player;
+    private String phase;
+
+    public int getPlayer(){
+        return this.player;
+    }
+
+    public String getPhase(){
+        return this.phase;
+    }
+
+    public void setPlayer(int player){
+        this.player=player;
+    }
+
+    public void setPhase(String phase){
+        this.phase=phase;
+    }
+
 }
